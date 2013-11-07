a0a3db8e46c09a293eeeb48eb8410a72c7f6e987
Eric Chan <chanman_ca@yahoo.com>
2013-11-07 00:26:24 -0500

    more interesting

--- Hello/src/main/java/io/github/er1/helloandroid/MainActivity.java
+++ Hello/src/main/java/io/github/er1/helloandroid/MainActivity.java
@@ -2,138 +2,26 @@ package io.github.er1.helloandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.ShareActionProvider;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
-    private ShareActionProvider mShareActionProvider;
-    protected String sharedText = "";
+    MainFragment mainfragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-
         Intent intent = getIntent();
-        String action = intent.getAction();
 
-        if (Intent.ACTION_SEND.equals(action)) {
-            sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        MenuItem item = menu.findItem(R.id.action_share);
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
-
-        if (mShareActionProvider != null) {
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_SEND);
-            intent.putExtra(Intent.EXTRA_TEXT, sharedText);
-            intent.setType("text/plain");
-            mShareActionProvider.setShareIntent(intent);
+        if (savedInstanceState == null) {
+            mainfragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, mainfragment).commit();
         }
 
-        return true;
-    }
-
-    private void setShareIntent(Intent shareIntent) {
-        if (mShareActionProvider != null) {
-            mShareActionProvider.setShareIntent(shareIntent);
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void btnHello(View view) {
-        Toast t = Toast.makeText(getApplicationContext(), R.string.hello_world, Toast.LENGTH_SHORT);
-        t.setGravity(Gravity.NO_GRAVITY, 0, 0);
-        t.show();
-    }
-
-    public void btnSwap(View view) {
-        TextView data = (TextView) findViewById(R.id.data);
-        TextView result = (TextView) findViewById(R.id.result);
-        CharSequence a = data.getText();
-        CharSequence b = result.getText();
-        data.setText(b);
-        result.setText(a);
-    }
-
-    public static class PlaceholderFragment extends Fragment {
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onViewCreated(View view, Bundle savedInstanceState) {
-            super.onViewCreated(view, savedInstanceState);
-            TextView tvdata = (TextView) getView().findViewById(R.id.data);
-            TextView tvresult = (TextView) getView().findViewById(R.id.result);
-            tvdata.addTextChangedListener(new TextProcessor(tvresult));
-            tvdata.setText(((MainActivity) getActivity()).sharedText);
-        }
-
-        class TextProcessor implements TextWatcher {
-            TextView target;
-
-            public TextProcessor(TextView target) {
-                this.target = target;
-            }
-
-            @Override
-            public void afterTextChanged(Editable e) {
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String data = s.toString();
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_TEXT, data);
-                intent.setType("text/plain");
-
-                ((MainActivity) getActivity()).setShareIntent(intent);
-                target.setText(Rot13.rot13(s));
-            }
+        if (Intent.ACTION_SEND.equals(intent.getAction())) {
+            mainfragment.setSharedText(intent.getStringExtra(Intent.EXTRA_TEXT));
         }
     }
 }
---
+++ Hello/src/main/java/io/github/er1/helloandroid/MainFragment.java
@@ -0,0 +1,152 @@
+package io.github.er1.helloandroid;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainFragment extends Fragment implements OnClickListener {
+    Button btnHello;
+    Button btnSwap;
+    Button btnShare;
+    TextView tvData;
+    TextView tvResult;
+    ListView lvList;
+    CharSequence sharedText = "";
+
+    public static String rot13(CharSequence in) {
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < in.length(); i++) {
+            char ch = in.charAt(i);
+            char c = Character.toUpperCase(ch);
+            if (c >= 'A' && c <= 'M')
+                ch += 13;
+            if (c >= 'N' && c <= 'Z')
+                ch -= 13;
+            out.append(ch);
+        }
+        return out.toString();
+    }
+
+    public static Cursor getTestCursor() {
+        final String[] names = {"Charlie", "Juliet", "Mike", "Oscar", "Romeo", "Victor"};
+
+        MatrixCursor cur = new MatrixCursor(new String[]{"_id", "val", "desc"});
+
+        int row = 0;
+        for (String name : names) {
+            cur.addRow(new Object[]{row, name, name.charAt(0) - 64 + ". " + name});
+            row++;
+        }
+
+        return cur;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        if (rootView == null)
+            return null;
+
+        btnHello = (Button) rootView.findViewById(R.id.btnhello);
+        btnSwap = (Button) rootView.findViewById(R.id.btnswap);
+        btnShare = (Button) rootView.findViewById(R.id.btnshare);
+        tvData = (TextView) rootView.findViewById(R.id.data);
+        tvResult = (TextView) rootView.findViewById(R.id.result);
+        lvList = (ListView) rootView.findViewById(R.id.list);
+
+        btnHello.setOnClickListener(this);
+        btnSwap.setOnClickListener(this);
+        btnShare.setOnClickListener(this);
+
+        tvData.addTextChangedListener(new TextProcessor(tvResult));
+        tvData.setText(sharedText);
+
+        SimpleCursorAdapter sca = new SimpleCursorAdapter(getActivity(),
+                android.R.layout.simple_list_item_2,
+                getTestCursor(),
+                new String[]{"val", "desc"},
+                new int[]{android.R.id.text1, android.R.id.text2},
+                0);
+
+        lvList.setAdapter(sca);
+
+        return rootView;
+    }
+
+    public void hello() {
+        Toast.makeText(getActivity(), R.string.hello_world, Toast.LENGTH_LONG).show();
+    }
+
+    public void swap() {
+        CharSequence a = tvData.getText();
+        CharSequence b = tvResult.getText();
+        tvData.setText(b);
+        tvResult.setText(a);
+    }
+
+    public void share() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, sharedText);
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnhello:
+                hello();
+                return;
+            case R.id.btnswap:
+                swap();
+                return;
+            case R.id.btnshare:
+                share();
+                return;
+            default:
+        }
+    }
+
+    public void setSharedText(CharSequence sharedText) {
+        this.sharedText = sharedText;
+        if (tvData != null) {
+            tvData.setText(this.sharedText);
+        }
+    }
+
+    class TextProcessor implements TextWatcher {
+        TextView target;
+
+        public TextProcessor(TextView target) {
+            this.target = target;
+        }
+
+        @Override
+        public void afterTextChanged(Editable e) {
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            sharedText = s;
+            target.setText(rot13(s));
+        }
+    }
+}
